f97bffd1c515dde82da76a4c0122e03a9536419c
Misel Evtimov <misel.evtimov@gmail.com>
2020-08-23 01:33:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,8 +28,8 @@ def turn(board)
     puts "Please enter 1-9:"
     answer = gets.chomp
     answer = input_to_index(answer)
-    if valid_move?(board, answer)
-      move(answer, board)
+    if valid_move?(answer, board)
+      move(board, answer)
       puts display_board(board)
     else
       puts "That is an invalid entry!"
